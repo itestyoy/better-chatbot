a92463a64dfb135783ce4d00d074530a1c352bcd
itestyoy <131544850+itestyoy@users.noreply.github.com>
2025-08-27 09:55:57 +0200

    BETTER_AUTH_URL

--- src/lib/auth/server.ts
+++ src/lib/auth/server.ts
@@ -27,8 +27,8 @@ const {
 
 export const auth = betterAuth({
   plugins: [nextCookies()],
-  baseURL: NEXT_PUBLIC_BASE_URL,
-  basePath: BASE_PATH + '/api/auth',
+  baseURL: NEXT_PUBLIC_BASE_URL + BASE_PATH,
+  basePath: '/api/auth',
   database: drizzleAdapter(pgDb, {
     provider: "pg",
     schema: {
