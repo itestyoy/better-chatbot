880208bfdecfb10469f24aaa5a64689ae5556957
itestyoy <131544850+itestyoy@users.noreply.github.com>
2025-08-27 12:32:32 +0200

    fix

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,6 +1,10 @@
 "use client";
 
 import { createAuthClient } from "better-auth/react"; // make sure to import from better-auth/react
+import { NEXT_PUBLIC_BASE_PATH, NEXT_PUBLIC_BASE_URL } from "lib/const";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+    baseURL: NEXT_PUBLIC_BASE_URL,
+    basePath: NEXT_PUBLIC_BASE_PATH + "/api/auth",
+});
 
