aaaaa5a2a7c7064d9cac3114460c1d4f5ccd38cd
itestyoy <131544850+itestyoy@users.noreply.github.com>
2025-08-27 11:12:22 +0200

    fix

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,3 @@
 import { auth } from "auth/server";
-import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth.handler);
+export const { GET, POST } = auth.handler;
